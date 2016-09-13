050b535d0d29abf2abc3039a1b991ce2b9bd80ff
Kevin Fox <kevin@efox.cc>
2016-09-13 16:08:39 -0700

    fix stashconfig

--- stash_config.sh
+++ stash_config.sh
@@ -5,7 +5,7 @@ if [ "x$1" == "x" ]; then
 fi
 if [ "$x" == "pull" ]; then
 	kubectl create secret generic kolla-config --from-file=globals.yml=/etc/kolla/globals.yml --from-file=passwords.yml=/etc/kolla/passwords.yml
-else if [ "$x" == "push" ]; then
+elif [ "$x" == "push" ]; then
 	kubectl get secret kolla-config -o json | jq -r '.data."passwords.yml"' | base64 -d > /etc/kolla/passwords.yml
 	kubectl get secret kolla-config -o json | jq -r '.data."globals.yml"' | base64 -d > /etc/kolla/globals.yml
 fi
